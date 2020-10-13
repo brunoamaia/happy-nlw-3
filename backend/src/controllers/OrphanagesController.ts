d1f7a5d8bfaa18bd53929de57b47f51956b5b0a7
brunoamaia <brunoamaia94@gmail.com>
2020-10-13 17:58:09 -0300

    acessar as imagens salvas

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import {Request, Response} from 'express';
 
 import {getRepository} from 'typeorm';
 import Orphanage from '../models/Orphanage';
+import orphanagesView from '../views/orphanages_view'
 
 export default {
   async create (request: Request, response: Response) {
@@ -40,17 +41,21 @@ export default {
 
   async index (request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanage)
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
 
-    return response.json(orphanages);
+    return response.json(orphanagesView.renderMany(orphanages));
   },
 
   async show (request: Request, response: Response) {
     const {id} = request.params;
     const orphanagesRepository = getRepository(Orphanage)
-    const orphanage = await orphanagesRepository.findOneOrFail();
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
 
-    return response.json(orphanage);
+    return response.json(orphanagesView.render(orphanage));
   }
 
 };
