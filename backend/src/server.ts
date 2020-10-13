4de10d07277e18a94193b9998e1efcf905f9bba1
brunoamaia <brunoamaia94@gmail.com>
2020-10-13 13:17:45 -0300

    conexão com o Banco de Dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import './database/connection'
 const app = express();
 app.use(express.json());
 
@@ -7,4 +8,4 @@ app.get('/users', (request, response) => {
   return response.json({ message: 'Hello World' });
 })
 
-app.listen(3333)
+app.listen(3333);
